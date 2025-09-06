00d73a74b38de20a2d3906b5f08d5e1cbb3f3f5b
Dawit Workeye <dawit.workye-ug@aau.edu.et>
2025-09-06 13:37:47 +0300

    feat(auth): improve login & signup flow
    - Updated axios configuration for API requests
    - Enhanced Login and SignUp pages with better form handling
    - Updated global styles in index.css
    - Refined auth state management in useAuthStore
    - Synced dependencies in package.json and lock file

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,126 @@
-import React from "react";
+import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare } from "lucide-react";
+import { useState, type FormEvent } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const { isLoggingIn, login } = useAuthStore();
+  const [FormData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [password, setShowPassword] = useState(false);
+  const validateForm = () => {
+    if (!FormData.email.trim()) return toast.error("Email required");
+    if (!/\S+@\S+\.\S+/.test(FormData.email))
+      return toast.error("Invalid email format");
+    if (!(FormData.password.length < 6)) return toast.error("");
+    if (!FormData.password.trim()) return toast.error("Password is required");
+    return true;
+  };
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const success = validateForm();
+    if (success === true) login(FormData);
+  };
+  return (
+    <div className="min-h-screen grid lg:grid-cols-2">
+      {/*left side
+       */}
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+        {/*logo */}
+        <div className="text-center mb-8">
+          <div className="flex flex-col items-center gap-2 group">
+            <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+              <MessageSquare className="size-6 text-primary" />
+            </div>
+            <h1 className="text-2xl font-bold mt-2">Welcome Back</h1>
+            <p className="text-base-content/60">Sign in to your account</p>
+          </div>
+        </div>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="form-control flex flex-col gap-2">
+            <label className="label ">
+              <span className="label-text font-medium">Email</span>
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center">
+                <Mail className="size-5 text-base-content/40" />
+              </div>
+              <input
+                type="email"
+                className="input input-bordered w-full pl-10"
+                placeholder="email@gmail.com"
+                value={FormData.email}
+                onChange={(e) =>
+                  setFormData({ ...FormData, email: e.target.value })
+                }
+              />
+            </div>
+          </div>
+          <div className="form-control flex flex-col gap-2">
+            <label className="label">
+              <span className="label-text font-medium">Password</span>
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center">
+                <Lock className="size-5 text-base-content/40" />
+              </div>
+              <input
+                type={password ? "password" : "text"}
+                className="input input-bordered w-full pl-10 outline-none"
+                placeholder="********"
+                value={FormData.password}
+                onChange={(e) =>
+                  setFormData({ ...FormData, password: e.target.value })
+                }
+              />
+              <button
+                type="button"
+                className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                onClick={() => setShowPassword(!password)}
+              >
+                {password ? (
+                  <EyeOff className="size-5 text-base-content/40" />
+                ) : (
+                  <Eye className="size-5 text-base-content/40" />
+                )}
+              </button>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary w-full"
+            disabled={isLoggingIn}
+          >
+            {isLoggingIn ? (
+              <>
+                <Loader2 className="size-5 animate-spin" />
+                Loading...
+              </>
+            ) : (
+              "Sign in"
+            )}
+          </button>
+        </form>
+        <div className="text-center pt-3">
+          <p className="text-base-content/60">
+            Don&apos;t have an account?
+            <Link to="/signup" className="link link-primary">
+              Create account
+            </Link>{" "}
+          </p>
+        </div>
+      </div>
+      {/*right side */}
+
+      {/* <AuthImagePattern
+          title="join our community"
+          subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+        /> */}
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,8 +1,11 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
 interface AuthStore {
-  authUser: null | object;
+  authUser: null;
   isSigningUp: boolean;
+  signup: (data: object) => void;
+  Login: (data: object) => void;
   isLoggingIn: boolean;
   isCheckingAuth: boolean;
   checkAuth: () => void;
@@ -15,6 +18,7 @@ export const useAuthStore = create<AuthStore>((set) => ({
   checkAuth: async () => {
     try {
       const response = await axiosInstance.get("/auth/check");
+      console.log(response);
       set({ authUser: response.data });
     } catch (error) {
       console.log("error in checkingAuth", error);
@@ -22,5 +26,21 @@ export const useAuthStore = create<AuthStore>((set) => ({
     } finally {
       set({ isCheckingAuth: false });
     }
+  },
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const response = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: response.data });
+    } catch (error) {
+      console.log(error);
+      toast.error(error);
+    }
+  },
+  Login: async (data) => {
+    try {
+      const response = await axiosInstance.post("/login", data);
+      set({ authUser: response.data });
+    } catch (error) {}
   },
 }));
